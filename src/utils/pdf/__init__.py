81d006fe75657a986181c4cd3d720c0d8d600175
Dmytro Kovtun <dm_ko@outlook.com>
2022-11-23 23:31:25 +0200

    feat: fitz usage

--- src/utils/pdf/__init__.py
+++ src/utils/pdf/__init__.py
@@ -1,3 +1,2 @@
-from .get_pdf_content import get_pdf_content
 from .get_pdf_content_ocr import get_pdf_content_ocr
 from .is_text_file import is_text_file
--- src/utils/pdf/get_pdf_content.py
+++ src/utils/pdf/get_pdf_content.py
@@ -10,7 +10,34 @@ logger = logging.getLogger(__name__.split(".")[-1])
 
 
 
-def get_pdf_content(filename: str, start_from: int = 0, boundaries: dict={}) -> Iterator[str]:
+def get_pdf_content_fitz(filename: str) -> Iterator[str]:
+    """Returns all text available in PDF file.
+
+    Args:
+        filename (str): Path to PDF file.
+
+    Returns:
+        Tuple[str, str]: Tuple of (content, error)
+    """
+    import fitz
+    doc = fitz.open(filename)
+    for page in doc:
+        # do something with 'page'
+        if page.search_for("55"):
+            # “text”: (default) plain text with line breaks. No formatting, no text position details, no images.
+            # “blocks”: generate a list of text blocks (= paragraphs).
+            # “words”: generate a list of words (strings not containing spaces).
+            # “html”: creates a full visual version of the page including any images. This can be displayed with your internet browser.
+            # “dict” / “json”: same information level as HTML, but provided as a Python dictionary or resp. JSON string. See TextPage.extractDICT() for details of its structure.
+            # “rawdict” / “rawjson”: a super-set of “dict” / “json”. It additionally provides character detail information like XML. See TextPage.extractRAWDICT() for details of its structure.
+            # “xhtml”: text information level as the TEXT version but includes images. Can also be displayed by internet browsers.
+            # “xml”: contains no images, but full position and font information down to each single text character. Use an XML module to interpret.
+            import json
+            # logger.info(json.dumps(page.get_text('blocks'), indent=4))
+            yield from [p[4] for p in page.get_text('blocks')]
+
+
+def get_pdf_content_pdfium(filename: str, start_from: int = 0, boundaries: dict={}) -> Iterator[str]:
     """Returns all text available in PDF file.
 
     Args:
@@ -23,7 +50,7 @@ def get_pdf_content(filename: str, start_from: int = 0, boundaries: dict={}) -> 
         pdf = pdfium.PdfDocument(filename)
         for index in range(start_from, len(pdf)):
             page = pdf[index]
-            width, height = page.get_size()
+            # width, height = page.get_size()
             # print(f'height {height}')
             textpage = page.get_textpage()
             #print(boundaries)
@@ -38,4 +65,3 @@ def get_pdf_content(filename: str, start_from: int = 0, boundaries: dict={}) -> 
     except IOError:
         error = f"File '{filename}' does not exist"
         logger.error(error)
-
