5fa286769d6300e1e21b8509aae156f60f5142b2
Dmytro Kovtun <dm_ko@outlook.com>
2022-11-24 23:55:45 +0200

    feat: get_pages_where_found usage

--- src/utils/pdf/get_pdf_content_from_text_ocr.py
+++ src/utils/pdf/get_pdf_content_from_text_ocr.py
@@ -10,17 +10,28 @@ from scrapy.utils.project import get_project_settings
 from os.path import basename
 from PIL import Image
 
+from utils.pdf.get_pages_where_found import get_pages_where_found
+
 settings = get_project_settings()
 PDF_TEMP_DIR_PATH = str(settings.get("PDF_TEMP_DIR_PATH"))
+POPPLER_PATH = str(settings.get("POPPLER_PATH"))
 
 if not exists(PDF_TEMP_DIR_PATH):
     makedirs(PDF_TEMP_DIR_PATH)
 
 
-def get_pdf_content_from_text_ocr(filename: str, bounding_func=None) -> Iterator[str]:
-    images = convert_from_path(filename, poppler_path=settings.get("POPPLER_PATH"))
+def get_pdf_content_from_text_ocr(filename: str, bounding_func=None, search_text: list = []) -> Iterator[str]:
+    images = convert_from_path(filename, poppler_path=POPPLER_PATH)
+
+    if search_text:
+        processable_pages = get_pages_where_found(filename, search_text)
+    else:
+        processable_pages = [i for i in range(len(images))]
 
     for i in range(len(images)):
+        if i not in processable_pages:
+            print(f"Skipping page {i} processing")
+            continue
         _orig_name = basename(filename)
         _full_name = join(PDF_TEMP_DIR_PATH, f"{_orig_name}_page_{str(i)}.jpg")
         images[i].save(_full_name, "JPEG")
@@ -31,7 +42,6 @@ def get_pdf_content_from_text_ocr(filename: str, bounding_func=None) -> Iterator
             title_image.save(_full_name, "JPEG")
 
         data = pytesseract.image_to_string(_full_name, lang="eng", config="-c preserve_interword_spaces=1")
+        remove(_full_name)
         yield data
-        # TODO
-        # remove(fname)
     return
