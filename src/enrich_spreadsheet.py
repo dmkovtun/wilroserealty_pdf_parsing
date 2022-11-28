cb530854577c76f9a9116001c6add9a1cd994c81
Dmytro Kovtun <dm_ko@outlook.com>
2022-11-28 20:47:10 +0200

    feat: log file usage

--- src/enrich_spreadsheet.py
+++ src/enrich_spreadsheet.py
@@ -1,4 +1,4 @@
 import os
 
-stream = os.popen("poetry run scrapy enrich_spreadsheet")
+stream = os.popen("poetry run scrapy enrich_spreadsheet > latest_processing.log 2>&1")
 output = stream.read()
