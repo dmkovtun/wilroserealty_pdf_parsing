993f62132cd118c2c385bb5c6f5ec5f1c04bb7b2
Dmytro Kovtun <dm_ko@outlook.com>
2022-11-29 17:48:49 +0200

    chore: log messages when process starts and ends

--- src/enrich_spreadsheet.py
+++ src/enrich_spreadsheet.py
@@ -1,4 +1,6 @@
 import os
 
+print("Started processing. Do not close this window.")
 stream = os.popen("poetry run scrapy enrich_spreadsheet > latest_processing.log 2>&1")
 output = stream.read()
+print("Processing completed successfully.")
