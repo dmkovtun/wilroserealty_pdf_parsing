dce96f648f308cea840b5b55da1a79eaa1665ec4
Dmytro Kovtun <dm_ko@outlook.com>
2022-12-27 20:27:56 +0200

    fix: docker python requirements installation

--- docker/ubuntu/setup.sh
+++ docker/ubuntu/setup.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 set -eu
-# bash
 chmod +x /var/app/src
-
-apt install tesseract-ocr -y
-apt install imagemagick -y
-apt install poppler-utils -y
-
-
 cd /var/app/src
-poetry install --no-dev --no-interaction --no-ansi
+
+poetry install --no-interaction --no-ansi --only main
 poetry run playwright install
 
-echo 'Everything was successfully installed'
-
-cd /var/app/pm2
-pm2 status
-# uncomment following line if pm2.config.js file existing in python/src dir and contain instructions
-# to run worker processes. Or replace with required bash terminal instructions
-pm2 start pm2.config.js
-
+python run_processing.py
 bash
